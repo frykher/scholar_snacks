ec7c49d00b9cf746000fe37bebf2e29b3716d265
Preston Williams <84547105+xNightmxreZ@users.noreply.github.com>
2024-01-16 21:54:18 -0500

    nav bar color

--- lib/Navigation.dart
+++ lib/Navigation.dart
@@ -48,7 +48,7 @@ class _NavigationState extends State<Navigation> {
               iconSize: 24,
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
               duration: Duration(milliseconds: 400),
-              tabBackgroundColor: Colors.grey[100]!,
+              tabBackgroundColor: Color.fromRGBO(52, 25, 83, 0.8)!,
               color: Colors.black,
               tabs: [
                 GButton(
